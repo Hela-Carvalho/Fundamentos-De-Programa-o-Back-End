1971576689edf021df6f747f10febc14208c0ac2
Hela-Carvalho <helamacarvalho97@gmail.com>
2025-09-16 20:10:20 -0500

    atividade tipos de dados

---
+++ TiposDeDados.java
@@ -0,0 +1,34 @@
+public class TiposDeDados {
+    public static void main(String[] args) {
+        byte idade = 19;
+        System.out.println("idade (tipo byte): " + idade);
+
+        short populacaoCidade = 2400;
+        System.out.println("População da cidade (tipo short): " + populacaoCidade);
+
+        int populacaoMundial = 700000000;
+        System.out.println("populaçã mundial (tipo int): " + populacaoMundial);
+
+        long distanciaEstrelas = 123;
+        System.out.println("distancia entre entrelas (tipo long): " + distanciaEstrelas);
+
+        float peso = 105.5f;
+        System.out.println("peso (tipo float): " + peso);
+
+        double pi = 3.14;
+        System.out.println("numero pi (tipo double): " + pi);
+
+        char inicialNome = 'H';
+        System.out.println("inicial do nome (tipo char): " + inicialNome);
+
+        boolean verdade = true;
+        boolean falso = false;
+        System.out.println("verdade (tipo boolean): " + verdade);
+        System.out.println("falso (tipo boolean): " + falso);
+        
+        String nomeCompleto = "Helã Matos De Carvalho";
+        System.out.println("nome completo (tipo string): " + nomeCompleto);
+    }
+
+    
+}
